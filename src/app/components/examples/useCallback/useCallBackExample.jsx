c562d35f6fe37026dd1c5aceccb4eae47fd33ded
Pavel Koryakin <unclekoin@gmail.com>
2021-11-19 18:16:09 +0300

    feat: add examples for useMemo & useCallback

--- src/app/components/examples/useCallback/useCallBackExample.jsx
+++ src/app/components/examples/useCallback/useCallBackExample.jsx
@@ -1,11 +1,53 @@
-import React from "react";
+import React, { useCallback, useEffect, useRef, useState } from "react";
 import CardWrapper from "../../common/Card";
 import SmallTitle from "../../common/typography/smallTitle";
 
 const UseCallBackExample = () => {
+    const [data, setData] = useState({});
+    const withOutCallback = useRef(0);
+    const withCallback = useRef(0);
+
+    const handleChange = ({ target }) => {
+        setData((prevState) => ({ ...prevState, [target.name]: target.value }));
+    };
+
+    const validateWithOutCallback = (data) => {
+        console.log(data);
+    };
+
+    const validateWithCallback = useCallback(() => (data) => {
+        console.log(data);
+    }, []);
+
+    useEffect(() => {
+        validateWithOutCallback(data);
+        validateWithCallback(data);
+    }, [data]);
+
+    useEffect(() => {
+        withOutCallback.current++;
+    }, [validateWithOutCallback]);
+
+    useEffect(() => {
+        withCallback.current++;
+    }, [validateWithCallback]);
+
     return (
         <CardWrapper>
+            <div>Render With Out Callback: {withOutCallback.current}</div>
+            <div>Render With Callback: {withCallback.current}</div>
             <SmallTitle>Example</SmallTitle>
+            <label htmlFor="email" className="form-label">
+                Email
+            </label>
+            <input
+                onChange={handleChange}
+                type="email"
+                name="email"
+                value={data.email || ""}
+                className="form-control mb-3"
+                id="email"
+            />
         </CardWrapper>
     );
 };
--- src/app/components/examples/useMemo/complexCalculate.jsx
+++ src/app/components/examples/useMemo/complexCalculate.jsx
@@ -1,15 +1,71 @@
-import React from "react";
+import React, { useEffect, useState, useMemo } from "react";
 import CardWrapper from "../../common/Card";
 import SmallTitle from "../../common/typography/smallTitle";
 
+const factorial = (n) => {
+    return n ? n * factorial(n - 1) : 1;
+};
+
+const runFactorial = (n) => {
+    console.log("Run Factorial");
+    return factorial(n);
+};
+
 const ComplexCalculateExample = () => {
+    const [value, setValue] = useState(100);
+    const [state, setState] = useState(false);
+
+    const result = useMemo(() => runFactorial(value), [value]);
+
+    const buttonColor = useMemo(
+        () => ({ value: state ? "warning" : "danger" }),
+        [state]
+    );
+
+    useEffect(() => {
+        console.log("Render");
+    });
+
+    useEffect(() => {
+        console.log("Render button color");
+    }, [buttonColor]);
+
+    const increaseValue = () => {
+        setValue((prevState) => prevState + 10);
+    };
+
+    const decreaseValue = () => {
+        setValue((prevState) => prevState - 10);
+    };
     return (
         <>
             <CardWrapper>
                 <SmallTitle>Кэширование сложных вычислений</SmallTitle>
+                <div className="mb-2 d-flex align-items-center">
+                    <span className="fs-4">!{value}</span>
+                    <span className="mx-1">=</span>
+                    <span>{result}</span>
+                </div>
+                <button onClick={increaseValue} className="btn btn-danger me-2">
+                    Increase
+                </button>
+                <button
+                    onClick={decreaseValue}
+                    className="btn btn-primary"
+                    disabled={!value}
+                >
+                    Decrease
+                </button>
             </CardWrapper>
             <CardWrapper>
                 <SmallTitle>Зависимость от сторонних setState</SmallTitle>
+                <div className="mb-2">State: {String(state)}</div>
+                <button
+                    onClick={() => setState((prevState) => !prevState)}
+                    className={`btn btn-${buttonColor.value}`}
+                >
+                    Change State
+                </button>
             </CardWrapper>
         </>
     );
